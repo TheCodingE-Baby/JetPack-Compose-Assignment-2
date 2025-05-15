41108f2e7a8b6204a041db0d4ba23db965d86b87
TheCodingE-Bany <eyobfik5@gmail.com>
2025-05-15 16:19:29 +0000

    Kotlin re-written.

--- app/src/main/java/com/example/jetpackcomposewithretrofitandroom/navigation/NavGraph.kt
+++ app/src/main/java/com/example/jetpackcomposewithretrofitandroom/navigation/NavGraph.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposewithretrofitandroom.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambda
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx. hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHost
@@ -18,13 +19,13 @@ fun NavGraph(){
         navController = navController,
         startDestination = "list"
     ){
-        composable("list") {
+        composableLambda("list") run {
             val viewModel = hiltViewModel<ToDoViewModel>()
             TodoListScreen(viewModel(), navController)
         }
-        composable("details/{toDoId}") { backStackEntry ->
-            val toDoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
-            ToDoDetails(toDoId)
+        composableLambda("details/{toDoId}") run  { backStackEntry ->
+        val toDoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
+        ToDoDetails(toDoId)
         }
     }
 }
